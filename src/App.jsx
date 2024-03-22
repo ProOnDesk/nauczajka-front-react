aa3b5e57396c9bd8702f532487156d89447347e4
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-03-22 14:01:18 +0100

    installed required libraries

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import AppLayout from './ui/AppLayout';
 import HomePage from './pages/HomePage';
 import InstructorsPage from './pages/InstructorsPage';
 import LoginPage from './pages/LoginPage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 0,
+		},
+	},
+});
 
 const router = createBrowserRouter([
 	{
@@ -18,7 +29,33 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+			<Toaster
+				position='top-center'
+				gutter={12}
+				containerStyle={{ margin: '8px' }}
+				toastOptions={{
+					success: {
+						duration: 3000,
+					},
+					error: {
+						duration: 3000,
+					},
+					style: {
+						zIndex: 1000,
+						fontSize: '16px',
+						maxWidth: '500px',
+						padding: '16px 20px',
+					},
+					className:
+						'bg-bgWhite dark:bg-bgDark1Hover text-bgDark dark:text-bgWhite',
+				}}
+			/>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,11 @@
+import toast from 'react-hot-toast';
+
 function LoginPage() {
+	const notify = () => toast.success('Here is your toast.');
 	return (
 		<div>
 			<p>Tutaj bedzie logowanie</p>
+			<button onClick={notify}>Make me a toast</button>
 		</div>
 	);
 }
