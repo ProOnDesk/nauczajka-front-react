0568a49603ac506573fcd5d9599feca17336adb1
viko4kka <kasprikviktoria4@gmail.com>
2024-04-08 16:25:31 +0200

    responsiveness up to 1054px

--- src/ui/homepage/HeaderButtonJoinNow.jsx
+++ src/ui/homepage/HeaderButtonJoinNow.jsx
@@ -3,7 +3,7 @@ import Button from "../Inputs/Button";
 function HeaderButtonJoinNow() {
 	return (
 		<>
-			<div className="flex flex-row p-12 z-10 justify-start items-center min-[375px]:p-16 min-[375px]:text-lg min-[535px]:text-xl min-[535px]:p-18 sm:text-2xl sm:p-20 md:m-8 md:text-xl">
+			<div className="flex flex-row p-12 z-10 justify-start items-center min-[375px]:p-16 min-[375px]:text-lg min-[535px]:text-xl min-[535px]:p-18 sm:text-2xl sm:p-20 md:m-8 md:text-2xl md:py-8 md:px-16  ">
 				<Button>Join Now</Button>
 			</div>
 		</>
--- src/ui/homepage/HeaderImg.jsx
+++ src/ui/homepage/HeaderImg.jsx
@@ -1,7 +1,7 @@
 function HeaderImg() {
 	return (
 		<>
-			<div className="absolute w-7/12 opacity-90 mt-80 ml-28 min-[375px]:w-8/12 min-[481px]:w-9/12 sm:top-[70px] sm:w-7/12 sm:right-[100px] md:mt-44 md:ml-80 md:w-2/4">
+			<div className="absolute w-7/12 opacity-90 mt-80 ml-28 min-[375px]:w-8/12 min-[481px]:w-9/12 sm:top-[70px] sm:w-7/12 sm:right-[100px] md:mt-28 md:ml-80 md:w-5/12">
 				<img
 					src="./girl.png "
 					alt="dziewczyna siedzi na pufie i trzyma na kolanach laptop"
