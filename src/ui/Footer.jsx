bfecb486d0417916b0fe8477b16746816a1794d2
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-04-09 10:54:21 +0200

    footer change color

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+
 function Footer() {
 	const currentYear = new Date().getFullYear();
+	const { pathname } = useLocation();
+	const [styles, setStyles] = useState('text-gray');
+
+	useEffect(() => {
+		if (pathname === '/') {
+			setStyles('text-white bg-mainPurple');
+		} else {
+			setStyles('text-gray');
+		}
+	}, [pathname]);
+
 	return (
-		<footer className='py-4 flex justify-center text-gray '>
+		<footer className={`py-4 flex justify-center ${styles}`}>
 			<p>{currentYear} &copy; Nauczajka</p>
 		</footer>
 	);
