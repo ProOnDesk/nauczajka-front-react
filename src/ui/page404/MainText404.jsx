9d1b630f75674c5273c340545cd652c663651b52
viko4kka <kasprikviktoria4@gmail.com>
2024-04-16 12:44:37 +0200

    changes in page404

--- src/ui/page404/MainText404.jsx
+++ src/ui/page404/MainText404.jsx
@@ -1,13 +1,13 @@
 function MainText404() {
 	return (
 		<div className="flex flex-col items-center w-full px-4">
-			<h1 className="text-7xl tracking-widest text-mainBlue pt-8 font-semibold  ">
-				404
-			</h1>
-
 			<h2 className="flex justify-center items-start w-full text-mainPurple text-4xl pt-10 px-12  font-medium min-[535px]:text-5xl">
 				Ooops!
 			</h2>
+
+			<h1 className="text-7xl tracking-widest text-mainBlue pt-8 font-semibold  ">
+				404
+			</h1>
 			<h3 className="w-full flex justify-center items-center pt-12 font-semibold text-mainPurple text-2xl   tracking-tighter min-[535px]:text-4xl	">
 				Strona nie znaleziona
 			</h3>
