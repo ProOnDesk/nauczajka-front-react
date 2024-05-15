6030d6394da9ba9641f50fd48a95da6cf3365a47
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-05-15 20:21:16 +0200

    add navigating to register page on homepage

--- src/ui/homepage/HeaderButtonJoinNow.jsx
+++ src/ui/homepage/HeaderButtonJoinNow.jsx
@@ -1,13 +1,17 @@
-import Button from "../Inputs/Button";
+import { NavLink } from 'react-router-dom';
+import Button from '../Inputs/Button';
 
 function HeaderButtonJoinNow() {
 	return (
 		<>
-			<div className="flex flex-row p-8 text-sm font-semibold  justify-start items-center  min-[375px]:p-14  min-[535px]:text-base min-[535px]:p-18 sm:text-lg sm:p-20 md:m-8  md:py-12 md:px-16 lg:px-10 lg:py-4 xl:text-xl xl:px-16  ">
-				<div className="z-10">
+			<NavLink
+				to={'/register'}
+				className='flex flex-row p-8 text-sm font-semibold  justify-start items-center  min-[375px]:p-14  min-[535px]:text-base min-[535px]:p-18 sm:text-lg sm:p-20 md:m-8  md:py-12 md:px-16 lg:px-10 lg:py-4 xl:text-xl xl:px-16  '
+			>
+				<div className='z-10'>
 					<Button>Dołącz teraz</Button>
 				</div>
-			</div>
+			</NavLink>
 		</>
 	);
 }
