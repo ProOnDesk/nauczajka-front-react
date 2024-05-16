415b05ae576b318ab7cf0e5fcba1436eb75f1c0b
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-05-16 22:40:01 +0200

    confirm email page prototype

--- src/pages/ConfirmEmailPage.jsx
+++ src/pages/ConfirmEmailPage.jsx
@@ -3,6 +3,7 @@ import { useVerifyUser } from '../features/Auth/useVerifyUser';
 import Button from '../ui/Inputs/Button';
 import { useContext, useEffect } from 'react';
 import { FooterContext } from '../context/FooterContext';
+import { CiAt } from 'react-icons/ci';
 
 function ConfirmEmailPage() {
 	const queryParams = new URLSearchParams(useLocation().search);
@@ -20,15 +21,27 @@ function ConfirmEmailPage() {
 	}
 
 	return (
-		<div>
-			<p>Stronka Do Potwierdzania E-mail</p>
-			<Button
-				disabled={isPending}
-				onClick={handleButton}
-				className='bg-mainPurple hover:bg-mainPurpleHover'
-			>
-				Potwierdź
-			</Button>
+		<div className='flex justify-center items-center max-w-7xl mx-auto '>
+			<div className='w-full md:w-1/2 rounded-md overflow-hidden md:shadow-myShadow'>
+				<div className='flex items-center justify-center w-full h-1/5 md:bg-mainPurple py-8'>
+					<span className='text-5xl md:text-white '>
+						<CiAt />
+					</span>
+				</div>
+				<div className='flex flex-col items-center gap-10 p-10'>
+					<p className='text-center text-base'>
+						Został tylko jeden krok, aby dokończyć rejestrację. Kliknij przycisk
+						i znajdź odpowiedniego dla Ciebie korepetytora!
+					</p>
+					<Button
+						disabled={isPending}
+						onClick={handleButton}
+						className='bg-mainPurple hover:bg-mainPurpleHover'
+					>
+						Potwierdź
+					</Button>
+				</div>
+			</div>
 		</div>
 	);
 }
